7e7cf52f7b775daf007bf06a0d7aa034d547e8b0
xiaoshen746 <doramart@qq.com>
2017-09-26 10:46:48 +0800

    添加广告模块

--- src/manage/components/Home.vue
+++ src/manage/components/Home.vue
@@ -14,7 +14,8 @@
                     <span class="el-dropdown-link userinfo-inner">
                         <img :src="loginState.userInfo.logo" /> {{loginState.userInfo.userName}}</span>
                     <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item @click.native="sysMessage">我的消息<el-badge v-show="loginState.noticeCounts > 0" class="mark" :value="loginState.noticeCounts" /></el-dropdown-item>
+                        <el-dropdown-item @click.native="sysMessage">我的消息
+                            <el-badge v-show="loginState.noticeCounts > 0" class="mark" :value="loginState.noticeCounts" /></el-dropdown-item>
                         <el-dropdown-item @click.native="sysSettings">设置</el-dropdown-item>
                         <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
                     </el-dropdown-menu>
@@ -24,7 +25,7 @@
         <el-col :span="24" class="main">
             <aside :class="collapsed?'menu-collapsed':'menu-expanded'">
                 <!--导航菜单-->
-                <el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect" unique-opened router v-show="!collapsed">
+                <el-menu v-if="!collapsed" :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect" unique-opened router v-show="!collapsed">
                     <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
                         <el-submenu :index="index+''" v-if="!item.leaf" :key="index">
                             <template slot="title">
@@ -36,7 +37,7 @@
                     </template>
                 </el-menu>
                 <!--导航菜单-折叠后-->
-                <ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
+                <ul class="el-menu el-menu-vertical-demo collapsed" v-else ref="menuCollapsed">
                     <li :key="index" v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
                         <template v-if="!item.leaf">
                             <div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
