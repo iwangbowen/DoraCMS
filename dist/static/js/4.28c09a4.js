3138acf1fbe5d52c710c6a819a661a5cda5c1a13
doramart <doramart@qq.com>
2019-08-06 21:59:16 +0800

    DoraCMS 2.1.3 😈😈😈

--- dist/static/js/4.28c09a4.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([4],{1066:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"AuthRedirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}},98:function(e,n,o){var i=o(6)(o(1066),null,null,null,null);e.exports=i.exports}});
